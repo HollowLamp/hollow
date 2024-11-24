b79d643b713f8027eb4e738eef10c67c8a8a0eaa
hollowlamp <2504384479@qq.com>
2024-11-24 20:48:59 +0800

    revert: 移除sitemap和robots

--- app/routes/_seo+/robots[.]txt.ts
+++
@@ -1,7 +0,0 @@
-import { generateRobotsTxt } from "@nasa-gcn/remix-seo";
-
-export function loader() {
-  return generateRobotsTxt([
-    { type: "sitemap", value: "https://star.hollowlamp.top/sitemap.xml" },
-  ]);
-}
